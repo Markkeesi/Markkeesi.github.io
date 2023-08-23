674f321fbbebc7b8296ec506baa1a6837695435a
Teemu Markkanen <122357967+Markkeesi@users.noreply.github.com>
2023-08-23 23:50:59 +0300

    about paivitys

--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
-function myFunction() {
-    var x = document.getElementById("header");
-    if (x.className === "header") {
-      x.className += " responsive";
-    } else {
-      x.className = "header";
-    }
+function hamburger() {
+  const menu = document.querySelector(".menuIcon");
+  menu.classList.toggle("change");
+
+  const navItems = document.querySelector(".nav-items");
+  navItems.classList.toggle("responsive");
+}
+
+window.onclick = function(e) {
+  if (!e.target.matches(".menuIcon, .bar1, .bar2, .bar3")) {
+    var navDrop = document.getElementById("navItems");
+      if (navDrop.classList.contains("responsive")) {
+        navDrop.classList.remove("responsive");
+      }
+    
+    var naviDrop = document.getElementById("menuIcon");
+      if (naviDrop.classList.contains("change")) {
+        naviDrop.classList.remove("change");
+      }
+  }
 }
